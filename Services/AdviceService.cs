8b228ce2ff2312a807a7f8472f53ceead38aa30a
Kawan Melo <kawangcmelo@gmail.com>
2024-07-24 18:06:39 -0300

    Adding advice service

---
+++ Services/AdviceService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Tokens;
+using SolomonsAdviceWebApp.Data;
+using SolomonsAdviceWebApp.Models.Entities;
+
+namespace SolomonsAdviceWebApp.Services
+{
+    public class AdviceService
+    {
+        protected readonly  ApplicationDbContext _context;
+
+        public AdviceService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Advice> GetRandomAsync()
+        {
+            var adviceList = await _context.Advices.ToListAsync();
+            if (!adviceList.IsNullOrEmpty())
+            {
+                Random random = new Random();
+                int randomNumber = random.Next(0, adviceList.Count);
+                var randomAdvice = adviceList[randomNumber];
+                return randomAdvice;
+            }
+            return null;
+        }
+    }
+}
