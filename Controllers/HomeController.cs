d9d87a5dd89ee10f96d2a3f050031eeeffd434d4
Kawan Melo <kawangcmelo@gmail.com>
2024-07-24 18:24:37 -0300

    Using AdviceService to get a random advice

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using SolomonsAdviceWebApp.Data;
 using SolomonsAdviceWebApp.Models;
+using SolomonsAdviceWebApp.Services;
 using System.Diagnostics;
 
 namespace SolomonsAdviceWebApp.Controllers
@@ -7,14 +9,19 @@ namespace SolomonsAdviceWebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AdviceService _adviceService;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, AdviceService adviceService)
         {
             _logger = logger;
+            _adviceService = adviceService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var randomAdvice = await _adviceService.GetRandomAsync();
+            ViewBag.RandomAdvice = randomAdvice;
             return View();
         }
 
