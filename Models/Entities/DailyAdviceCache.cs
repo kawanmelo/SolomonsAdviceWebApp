cbd070935fcd43d492200bb5545133c3af26689f
Kawan Melo <kawangcmelo@gmail.com>
2024-07-26 21:18:01 -0300

    Adding class to static advice

---
+++ Models/Entities/DailyAdviceCache.cs
@@ -0,0 +1,13 @@
+﻿namespace SolomonsAdviceWebApp.Models.Entities
+{
+    public static class DailyAdviceCache
+    {
+        private static Advice _dailyAdvice;
+
+        public static Advice DailyAdvice
+        {
+            get { return  _dailyAdvice; }
+            set { _dailyAdvice = value; }
+        }
+    }
+}
