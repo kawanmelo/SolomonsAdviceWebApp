75447ad19fa7518d04c78299343ec2d528527061
Kawan Melo <kawangcmelo@gmail.com>
2024-08-02 22:55:04 -0300

    Create model to get favorite advices

---
+++ Models/FavoriteAdvice.cs
@@ -0,0 +1,20 @@
+﻿using SolomonsAdviceWebApp.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SolomonsAdviceWebApp.Models
+{
+    public class FavoriteAdvice
+    {
+        [Key]
+        [Required]
+        [MaxLength(450)]
+        public string UserId { get; set; }
+
+
+        [Required]
+        public int AdviceId { get; set; }
+
+
+    }
+}
