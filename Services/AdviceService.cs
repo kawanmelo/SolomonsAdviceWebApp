8509a5f018496a2673346fe43218e9ce3d7bc6b9
Kawan Melo <kawangcmelo@gmail.com>
2024-08-02 22:56:32 -0300

    Adding service to get favorite advices

--- Services/AdviceService.cs
+++ Services/AdviceService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Client;
 using Microsoft.IdentityModel.Tokens;
@@ -41,6 +42,26 @@ namespace SolomonsAdviceWebApp.Services
                 int randomNumber = random.Next(0, adviceList.Count);
                 var randomAdvice = adviceList[randomNumber];
                 return randomAdvice;
+            }
+            return null;
+        }
+
+        public async Task<List<Advice>> GetFavoritesAsync(string id)
+        {
+            var favoriteAdvicesReferences = await _context.FavoriteAdvices.Where(x => x.UserId == id).ToListAsync();
+
+            if (!favoriteAdvicesReferences.IsNullOrEmpty())
+            {
+                List<Advice> advicesFound = new List<Advice>();
+                foreach (var fav in favoriteAdvicesReferences)
+                {
+                    var advice = _context.Advices.FirstOrDefault(x => x.Id == fav.AdviceId);
+                    if (advice != null)
+                    {
+                        advicesFound.Add(advice);
+                    }
+                }
+                return advicesFound;
             }
             return null;
         }
