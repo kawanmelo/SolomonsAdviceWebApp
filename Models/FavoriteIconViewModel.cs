9396e656c4f1594fbd706b9fddc30bdaf5c84de9
Kawan Melo <kawangcmelo@gmail.com>
2024-08-08 20:29:44 -0300

    Refactoring actions and views responsible for toggle favorite status

---
+++ Models/FavoriteIconViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SolomonsAdviceWebApp.Models
+{
+    public class FavoriteIconViewModel
+    {
+        public int AdviceId { get; set; }
+        public bool IsFavorite { get; set; }
+    }
+}
